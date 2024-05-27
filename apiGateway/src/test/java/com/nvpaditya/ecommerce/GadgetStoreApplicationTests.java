0d3baee12d853bf10704182c3acff50ecfe0893d
Aditya <nvp956@gmail.com>
2024-05-27 18:30:12 -0400

    gadget Store User Service and API Gateway

---
+++ apiGateway/src/test/java/com/nvpaditya/ecommerce/GadgetStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nvpaditya.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GadgetStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
